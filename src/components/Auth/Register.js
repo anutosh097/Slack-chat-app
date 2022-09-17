fbbf5b1be30b24e38cf6d773179ecda287b2e3d6
anutosh097 <anutosh053@gmail.com>
2022-09-17 21:32:39 +0530

    adding new users to firebase with email and password from registration form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,31 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Grid,Form,Segment,Button,Header,Message,Icon} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
+import firebase from "../../firebase";
 
 const Register = () => {
 
+ const [username, handleUser] = useState("")
+ const [email, handleEmail] = useState("")
+ const [password, handlePassword] = useState("")
+ const [passwordConfirmation, handlePasswordConfirmation] = useState("")
 
- const handleChange = () => {}
+ const handleSubmit = event => {
+    event.preventDefault()
+    firebase
+     .auth()
+     .createUserWithEmailAndPassword(email, password)
+     .then(userCredential => console.log(userCredential))
+     .catch(err => console.log(err))
+ }
 
   return (
+    <React.StrictMode>
     <Grid textAlign = "center" verticalAlign="middle" className="app">
        <Grid.Column style={{maxWidth: 450}}>
           <Header as="h2" icon color="orange" textAlign="center">
              <Icon name="puzzle piece" color="orange" />
                Register for DevChat             
           </Header>
-          <Form size="large">
+          <Form size="large" onSubmit={handleSubmit}>
             <Segment stacked>
-               <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={handleChange} type="text"/>
-               <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="EMail Address" onChange={handleChange} type="email"/>
-               <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={handleChange} type="password"/>
-               <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={handleChange} type="password"/>
+               <Form.Input fluid name="username" icon="user" iconPosition="left" value={username} placeholder="Username" onChange={e => handleUser(e.target.value)} type="text"/>
+               <Form.Input fluid name="email" icon="mail" iconPosition="left" value={email} placeholder="Email Address" onChange={e => handleEmail(e.target.value)} type="email"/>
+               <Form.Input fluid name="password" icon="lock" iconPosition="left" value={password} placeholder="Password" onChange={e => handlePassword(e.target.value)} type="password"/>
+               <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" value={passwordConfirmation} placeholder="Password Confirmation" onChange={e => handlePasswordConfirmation(e.target.value)} type="password"/>
                <Button color="orange" fluid size="large">Submit</Button>
             </Segment>
           </Form>
           <Message>Already a user? <Link to="/login">Login</Link></Message>
        </Grid.Column>
     </Grid>
+    </React.StrictMode>
   )
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
-import { getDatabase } from "firebase/database";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,14 +19,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-// Initialize Cloud Storage and get a reference to the service
-const storage = getStorage(app);
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+firebase.initializeApp(firebaseConfig);
+
 
 
 
 export default firebase;
+ 
